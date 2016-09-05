08cf2d86457aed283aac71e48b92f066deb819c9
beckrecca <rebecca.doris42@gmail.com>
2016-09-05 17:33:01 -0400

    hikes table seeded

---
+++ database/seeds/HikesTableSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class HikesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // Retrieve JSON file of hikes
+        $json = File::get("database/data/hikes.json");
+        // Decode the JSON data
+        $data = json_decode($json); 
+        // for each hike, insert into table
+        foreach ($data as $obj) {
+        	/**
+        	* Adapted from Sam Deering's article
+        	* http://www.fullstack4u.com/laravel/laravel-5-load-seed-data-from-json/
+        	**/
+	         DB::table('hikes')->insert([
+		        'name' => $obj->name,
+		        'path_name' => $obj->path_name,
+		        'description' => $obj->description,
+		        'level' => $obj->level,
+		        'web' => $obj->web
+	    	]);
+   		 }
+    }
+}
