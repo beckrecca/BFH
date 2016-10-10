8838f6422e9e27892bcfe17137b5a1621993e9a4
beckrecca <rebecca.doris42@gmail.com>
2016-10-10 13:37:37 -0400

    added cookie address to map page

--- resources/views/hikes/map.blade.php
+++ resources/views/hikes/map.blade.php
@@ -87,6 +87,16 @@
   <script src="/js/map.js"></script>
   <script src="/js/haversine.js"></script>
   <script src="/js/usercookie.js"></script>
+  <!-- user address cookie -->
+   <script>
+      // get the address
+      var address = readAddress();
+      // if there is an address cookie
+      if (address != null) {
+        // pre-fill it in the find nearest hike form
+        $('#user').val(address);
+      }
+   </script>
   <!-- Google Maps Javascript API -->
   <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAfIWxFiTBaolXUvFobvatofTwGKuEYaKA&callback=initMap"
     async defer></script>
